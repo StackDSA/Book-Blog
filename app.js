76c22e34fbb23bc8260f2d1b34214d95dfda86c8
Ishan Deshpande <ishangreat01@gmail.com>
2021-12-23 16:19:11 +0530

    Final Commit

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://Ishan01:IDCode_01@cluster0.xnonp.mongodb.net/blogDB", {useNewUrlParser: true});
+const password = process.env.PASSWORD;
+mongoose.connect("mongodb+srv://Ishan01:PASSWORD@cluster0.xnonp.mongodb.net/blogDB", {useNewUrlParser: true});
 const postSchema = {
  author: String,
  released: String,
